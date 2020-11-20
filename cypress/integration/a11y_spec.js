ebcc9f6259c57178b11d8f14a6c7f7cc7a39ff7f
Alvaro Montoro <alvaromontoro@gmail.com>
2020-11-19 23:48:42 -0600

    Add tests for headings

--- cypress/integration/a11y_spec.js
+++ cypress/integration/a11y_spec.js
@@ -91,4 +91,33 @@ describe('Checka11y.css Tests', () => {
         .should("eq", "ERROR: html has no lang attribute.");
     });
   });
+
+  describe("<headings>", () => {
+    it('should not have empty headings', () => {
+      cy.get("h1:empty, h2:empty, h3:empty, h4:empty, h5:empty, h6:empty")
+        .each(element => {
+          cy.get(element)
+            .after("content")
+            .should('eq', "ERROR: Headings must not be empty.")
+        });
+    });
+
+    it('should be accessible to assistive technologies', () => {
+      cy.get("h1[aria-hidden], h2[aria-hidden], h3[aria-hidden], h4[aria-hidden], h5[aria-hidden], h6[aria-hidden]")
+        .each(element => {
+          cy.get(element)
+            .after("content")
+            .should('eq', "ERROR: Headings should be reachable by assistive technologies (no aria-hidden).")
+        });
+    });
+
+    it('should be accessible to assistive technologies', () => {
+      cy.get("#heading-skip-levels h1, #heading-skip-levels h4, #heading-skip-levels h5, #heading-skip-levels h6")
+        .each(element => {
+          cy.get(element)
+            .after("content")
+            .should('eq', "ERROR: Headings should not skip levels.")
+        });
+    });
+  });
 });
