81281fe26d65e35e32af1276b03e6e77f67e1170
Alvaro Montoro <alvaromontoro@gmail.com>
2020-12-02 08:08:26 -0600

    Check order headings (#82)
    * Fix headings bug
    
    Missing & does not select proper elements
    
    * Headings: test skipping levels
    
    Code + demo + generated library
    
    * Add tests for headings
    
    * Update features.md
    
    * Remove !important to avoid conflict
    
    * Add cases for incorrect headings
    
    - Add new cases to improve coverage
    - Update tests to make a bit more generic in 2 cases
    - Build

--- cypress/integration/a11y_spec.js
+++ cypress/integration/a11y_spec.js
@@ -91,4 +91,33 @@ describe('Checka11y.css Tests', () => {
         .should("eq", "ERROR: html has no lang attribute.");
     });
   });
+
+  describe("<headings>", () => {
+    it('should not have empty headings', () => {
+      cy.get("h1:empty, h2:empty, h3:empty, h4:empty, h5:empty, h6:empty")
+        .each(element => {
+          cy.get(element)
+            .after("border-color")
+            .should('eq',  'rgb(255, 0, 0)') // they could have different error messages
+        });
+    });
+
+    it('should be accessible to assistive technologies', () => {
+      cy.get("h1[aria-hidden], h2[aria-hidden], h3[aria-hidden], h4[aria-hidden], h5[aria-hidden], h6[aria-hidden]")
+        .each(element => {
+          cy.get(element)
+            .after("border-color")
+            .should('eq',  'rgb(255, 0, 0)') // they could have different error messages
+        });
+    });
+
+    it('should be accessible to assistive technologies', () => {
+      cy.get("#heading-skip-levels h1, #heading-skip-levels h4, #heading-skip-levels h5, #heading-skip-levels h6")
+        .each(element => {
+          cy.get(element)
+            .after("content")
+            .should('eq', "ERROR: Headings should not skip levels.")
+        });
+    });
+  });
 });
