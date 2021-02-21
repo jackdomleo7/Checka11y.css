5f8652a9545ac4f254019b8065c7fb9353f11446
Alvaro Montoro <alvaromontoro@gmail.com>
2021-02-21 11:29:04 -0600

    Add tests for [dir]
    - Add tests for dir
    - Update examples to cover valid and invalid test cases

---
+++ cypress/integration/dir_spec.js
@@ -0,0 +1,23 @@
+describe("[dir]", () => {
+  before(() => {
+    cy.visit("/test/index.html");
+  });
+
+  it('should show error on elements with wrong values for dir attribute', () => {
+    cy.get("[dir]:not([dir='rtl']):not([dir='ltr']):not([dir='auto'])")
+      .each(element => {
+        cy.get(element)
+          .after('content')
+          .should('eq', "ERROR: The dir attribute can only have the values, 'ltr', 'rtl' and 'auto'.");
+      });
+  });
+
+  it('should not show error on elements with correct dir values', () => {
+    cy.get("[dir='ltr'], [dir='rtl'], [dir='auto']")
+      .each(element => {
+        cy.get(element)
+          .after('content')
+          .should('not.contain', "ERROR:");
+      });
+  });
+});
