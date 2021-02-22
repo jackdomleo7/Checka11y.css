4211fb9d9b3f4d12b03dd78ab6409ae5bed2e7a6
Alvaro Montoro <alvaromontoro@gmail.com>
2021-02-21 19:26:07 -0600

    Create inline_spec.js

---
+++ cypress/integration/inline_spec.js
@@ -0,0 +1,23 @@
+describe("[inline]", () => {
+  before(() => {
+    cy.visit("/test/index.html");
+  });
+
+  it('should not have inline styles with !important', () => {
+    cy.get("[style*='!important']")
+      .each(element => {
+        cy.get(element)
+          .after('content')
+          .should('eq', "WARNING: !important inline styles should be avoided.");
+      });
+  });
+
+  it('should have role="text" if it contains em or span', () => {
+    cy.get(":not([role='text']) span, :not([role='text']) em")
+      .each(element => {
+        cy.get(element)
+        .after('content')
+        .should('eq', "WARNING: Text broken by <span> or <em> elements might need a [role='text'] attribute.");
+      });
+  });
+});
