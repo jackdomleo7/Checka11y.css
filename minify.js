80d0cbc0b81478836c322eba1e131059c54cd17f
Tanner <tannercdolby@gmail.com>
2021-11-05 21:45:07 -0700

    Add minify.js to handle minifying stylesheets

---
+++ minify.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const CleanCSS = require("clean-css");
+const stylesheets = ["checka11y.css", "checka11y-errors.css", "checka11y-warnings.css"];
+
+console.log("Running clean-css!");
+stylesheets.forEach(filename => {
+    let minifiedFile = `${filename.slice(0, -4)}-minified.css`;
+    console.log(`Generating ${minifiedFile}`);
+    fs.readFile(`./${filename}`, "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        // todo: replace {} with config options if we want (e.g use beautify or other formatting options)
+        // https://www.npmjs.com/package/clean-css#formatting-options
+        let styles = new CleanCSS({}).minify(data).styles;
+        fs.writeFile(`./${minifiedFile}`, styles, (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+        });
+    });
+});
